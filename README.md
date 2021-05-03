d4ca09009e99611f62675d663f09f9dde43ed597
Jon Connell <github@figsandfudge.com>
2021-05-03 09:39:00 +0100

    Added build error checking

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 proto-dump
-==========================
+================
 
 proto-dump is a tool for recovering [Protobuf](https://code.google.com/p/protobuf/) descriptors (.proto files) from compiled programs. It can be thought of as [class-dump](http://stevenygard.com/projects/class-dump) for Protobuf.
 
-Usage
------
+
+## Updates
+
+This fork has had the following small updates since being forked from [Sean Patrick O'Brien's](http://www.obriensp.com)
+version at https://github.com/obriensp/proto-dump:
+
+* included a version of Google Protobuf 2.5.0
+* added patches to build the Google Protobuf and Command Line Utilities dependencies on recent macOS including Apple Silicon
+
+
+An example use of ```proto-dump``` can be found at https://github.com/masaccio/numbers-parser for extracting protocol buffers from
+Apple Numbers executables. In particular it appears necessary to pull all frameworks and dynamically loaded code into a single file
+for ```proto-dump``` to extract. An example of this can be found in https://github.com/masaccio/numbers-parser/blob/master/protos/extract_protos.sh.
+
+
+## Usage
+```
 	proto-dump 0.1
 	Usage: proto-dump [-hv] [-o <output>] <input>
 	  -h, --help                Show usage information and exit
 	  -v, --version             Show version information
 	  -o, --output=<output>     Write the .proto files to <output>
 	  <input>                   Extract Protobuf descriptors from <input>
+```
--- build.sh
+++ build.sh
@@ -1,4 +1,40 @@
-xcodebuild VALID_ARCHS=`arch` ONLY_ACTIVE_ARCH=YES -configuration Release -project proto-dump.xcodeproj -target proto-dump clean
-xcodebuild VALID_ARCHS=`arch` ONLY_ACTIVE_ARCH=YES -configuration Release -project proto-dump.xcodeproj -target proto-dump build
-xcodebuild VALID_ARCHS=`arch` ONLY_ACTIVE_ARCH=YES -configuration Release -project proto-dump.xcodeproj -target ProtoDumpUnitTests build
-xcodebuild VALID_ARCHS=`arch` ONLY_ACTIVE_ARCH=YES -configuration Release -project proto-dump.xcodeproj -scheme proto-dump test
+#! /bin/bash
+
+exec 3>&1 4>&2 >build.log 2>&1
+
+function fatal_error {
+  echo $(tput setaf 1)"*** Build failed: examine build.log" $(tput sgr0) >&3
+  exit 1
+}
+
+PROTOBUF_AR="Externals/Protobuf/prebuilt/lib/libprotobuf.a"
+CLU_AR="Externals/CLU/build/Release/libCommand Line Utilities.a"
+if [ ! -f "${PROTOBUF_AR}" ]; then
+    echo $(tput setaf 1)*** ${PROTOBUF_AR}: archive not found$(tput sgr0) >&4
+	echo $(tput setaf 2)"*** Run build.sh in Externals/Protobuf"$(tput sgr0) >&3
+	exit 1
+fi
+if [ ! -f "${CLU_AR}" ]; then
+    echo $(tput setaf 1)*** ${CLU_AR}: archive not found$(tput sgr0) >&4
+	echo $(tput setaf 2)"*** Run build.sh in Externals/CLU"$(tput sgr0) >&3
+	exit 1
+fi
+
+args="VALID_ARCHS=`arch` ONLY_ACTIVE_ARCH=YES -configuration Release -project proto-dump.xcodeproj"
+
+echo $(tput setaf 2)"*** Logging to build.log" $(tput sgr0) >&3
+echo $(tput setaf 2)"*** Cleaning build environment" $(tput sgr0) >&3
+xcodebuild ${args} -target proto-dump clean || fatal_error "Clean failed: check build.log"
+
+echo $(tput setaf 2)"*** Building `arch` architecture" $(tput sgr0) >&3
+xcodebuild ${args} -target proto-dump build || fatal_error "Build failed: check build.log"
+
+echo $(tput setaf 2)"*** Building unit tests" $(tput sgr0) >&3
+xcodebuild ${args} -target ProtoDumpUnitTests build || fatal_error "Tests build failed: check build.log"
+
+echo $(tput setaf 2)"*** Running unit tests" $(tput sgr0) >&3
+xcodebuild ${args} -scheme proto-dump test || fatal_error "Test failed: check build.log"
+
+echo $(tput setaf 2)"*** Build and test successful!" $(tput sgr0) >&3
+echo "% build/Release/proto-dump" >&3
+build/Release/proto-dump >&3
